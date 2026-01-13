a25ed77093d86c55e177f775a854a1ede2523ccf
Gokul-Dev <gokulad2021008@gmail.com>
2026-01-13 22:59:03 +0530

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,57 @@
-
 import os
 import sys
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.execption import customException
 
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise customException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+
+            #  Skip GridSearch for CatBoost
+            if model_name == "CatBoosting Regressor":
+                model.fit(X_train, y_train)
+            else:
+                gs = GridSearchCV(model, param[model_name], cv=3)
+                gs.fit(X_train, y_train)
+
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise customException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise customException(e, sys)
